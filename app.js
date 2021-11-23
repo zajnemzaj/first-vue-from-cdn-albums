5fe9619cdc6e3bdd44e2627d885e4680808ce277
zajnemzaj <dezsoj@gmail.com>
2021-11-23 21:36:43 +0100

    passing argument and event type

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ const app = Vue.createApp({
         toggleShowBooks() {
             this.showBooks = !this.showBooks
         },
-        handleEvent() {
-            console.log('event')
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
         }
     }
 })
