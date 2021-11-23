a1978c6629329b8f7af2cd6e802ba1d770d746a4
zajnemzaj <dezsoj@gmail.com>
2021-11-23 21:40:32 +0100

    getting back mouse position with mousemove event

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
@@ -16,6 +18,10 @@ const app = Vue.createApp({
             if (data) {
                 console.log(data)
             }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
