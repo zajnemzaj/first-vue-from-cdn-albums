38395783a6af3612ad6a02befe65c1212b01a835
zajnemzaj <dezsoj@gmail.com>
2021-11-23 21:05:46 +0100

    calling method from click event

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = Vue.createApp({
             author: 'Brandon Sanderson',
             age: 45
         }
+    },
+    methods: {
+        changeTitle() {
+            this.title = 'Words of Randiance'
+        }
     }
 })
 
