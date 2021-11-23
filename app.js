46a93a8dd23d030d7f4e1d65789f206011634584
zajnemzaj <dezsoj@gmail.com>
2021-11-23 21:28:42 +0100

    fireing mouseover event

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = Vue.createApp({
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent() {
+            console.log('event')
         }
     }
 })
