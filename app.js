3391a8c0f0c31705830622789be2f830f94cec78
zajnemzaj <dezsoj@gmail.com>
2021-11-24 08:45:50 +0100

    v-bind images from data

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ const app = Vue.createApp({
             showBooks: true,
             books: [{
                     title: 'name of the wind',
-                    author: 'patric rothfuss'
+                    author: 'patric rothfuss',
+                    img: 'assets/1.jpg'
                 },
                 {
                     title: 'the way of kings',
-                    author: 'brandon sanderson'
+                    author: 'brandon sanderson',
+                    img: 'assets/2.jpg'
                 },
                 {
                     title: 'the final empire',
-                    author: 'brandon sanderson'
+                    author: 'brandon sanderson',
+                    img: 'assets/3.jpg'
                 }
             ]
         }
