2a9aadc01a6e48aa7334a5d6d348e45c27ead325
zajnemzaj <dezsoj@gmail.com>
2021-11-24 08:32:14 +0100

    displaying array elements

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45,
-            x: 0,
-            y: 0
+            books: [{
+                    title: 'name of the wind',
+                    author: 'patric rothfuss'
+                },
+                {
+                    title: 'the way of kings',
+                    author: 'brandon sanderson'
+                },
+                {
+                    title: 'the final empire',
+                    author: 'brandon sanderson'
+                }
+            ]
         }
     },
     methods: {
