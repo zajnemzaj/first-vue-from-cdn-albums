9f6781bab38047ea481a1dea1c13d5e40a943426
zajnemzaj <dezsoj@gmail.com>
2021-11-24 08:56:50 +0100

    v-bind conditional class attach based on data boolean value

--- app.js
+++ app.js
@@ -6,17 +6,20 @@ const app = Vue.createApp({
             books: [{
                     title: 'name of the wind',
                     author: 'patric rothfuss',
-                    img: 'assets/1.jpg'
+                    img: 'assets/1.jpg',
+                    isFav: true
                 },
                 {
                     title: 'the way of kings',
                     author: 'brandon sanderson',
-                    img: 'assets/2.jpg'
+                    img: 'assets/2.jpg',
+                    isFav: false
                 },
                 {
                     title: 'the final empire',
                     author: 'brandon sanderson',
-                    img: 'assets/3.jpg'
+                    img: 'assets/3.jpg',
+                    isFav: true
                 }
             ]
         }
